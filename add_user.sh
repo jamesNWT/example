0d98003f27e9be58c6ceef37b478470fd4082c08
James Wilcox <jameswilcox.98@gmail.com>
2021-11-16 10:49:55 -0400

    debug 2

--- add_user.sh
+++ add_user.sh
@@ -3,6 +3,6 @@
 groupadd -o -g 121 docker
 useradd -o -m -g 121 -u 1001 runner
 
-chown -R runner:docker /home/reports
+# chown -R runner:docker /home/reports
 
 su runner -c 'touch /home/reports/test.txt'
